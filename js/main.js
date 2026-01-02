e67e023cee70ad1b66884e772b4b8ae0e718b03b
hugobaires69 <hugobaires69@yahoo.com.ar>
2026-01-02 08:23:38 -0300

    Add basic JavaScript interaction with DOM events

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-console.log("Proyecto frontend iniciado correctamente");
+const button = document.getElementById("actionBtn");
+const message=document.getElementById("message");
+
+button.addEventListener("click", ()=>{
+    message.textContent="Gracias por visitar mi proyecto frontend!";
+});
