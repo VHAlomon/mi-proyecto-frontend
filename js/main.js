b7cc2d873eca44050e93d276c17c4ee1fc054be9
hugobaires69 <hugobaires69@yahoo.com.ar>
2026-01-02 11:16:39 -0300

    Agrego cambios en el archivo main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
+
 const button = document.getElementById("actionBtn");
 const message = document.getElementById("message");
+const MESSAGE_TEXT = "¡Gracias por visitar mi proyecto frontend!";
 
-button.addEventListener("click", () => {
-  message.textContent = "Gracias por visitar mi proyecto frontend!";
-  button.textContent = "Mensaje enviado";
-  button.disabled = true;
-});
+
+function handleButtonClick() {
+    message.textContent=MESSAGE_TEXT;
+    button.textContent = "Mensaje enviado";
+    button.disabled = true;
+}
+
+
+button.addEventListener("click", handleButtonClick);
